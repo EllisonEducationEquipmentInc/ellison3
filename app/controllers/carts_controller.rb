4872e9031ccad69d8752ef896d4d9fa177db46c3
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2010-09-07 19:54:44 +0000

    checkout

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -21,9 +21,35 @@ class CartsController < ApplicationController
 	def checkout
 		@title = "Checkout"
 		@cart_locked = true
-		@payment = Payment.new
-		@payment.attributes = get_user.billing_address.attributes if get_user.billing_address
+		unless get_user.billing_address && get_user.shipping_address
+			@shipping_address = get_user.shipping_address || get_user.addresses.build(:address_type => "shipping") 
+			@billing_address = get_user.billing_address || get_user.addresses.build(:address_type => "billing") 
+		end
+		new_payment
 		redirect_to(products_path, :alert => I18n.t(:empty_cart)) and return if get_cart.cart_items.blank?
 	end
 	
+	def create_shipping
+		@shipping_address = get_user.addresses.build(:address_type => "shipping")
+		@shipping_address.attributes = params[:shipping_address]
+		@billing_address = get_user.addresses.build(:address_type => "billing")
+	end
+	
+	def create_billing
+		@billing_address = get_user.addresses.build(:address_type => "billing")
+		@billing_address.attributes = params[:billing_address]
+		new_payment
+	end
+	
+	def copy_shipping_address
+		@billing_address = get_user.addresses.build(get_user.shipping_address.attributes)
+		@billing_address.address_type = "billing"
+	end
+
+private
+	
+	def new_payment
+		@payment = Payment.new
+		@payment.attributes = get_user.billing_address.attributes if get_user.billing_address
+	end
 end
