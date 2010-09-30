1398dff1c5a1398cb67d15a8cd76f3f0c4dfe651
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2010-09-30 19:16:51 +0000

    solr catalog

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -26,7 +26,7 @@ class IndexController < ApplicationController
 	    Tag::TYPES.each do |e|
     		query.facet :"#{e.to_s.pluralize}_#{current_system}"
      	end
-     	query.paginate(:page => 1, :per_page => 24)
+     	query.paginate(:page => params[:page] || 1, :per_page => 24)
 	  end
 	  @products = @search.results
 	end
