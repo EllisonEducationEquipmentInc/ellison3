189b485f419c3a11acfbf1aaabd32e9b63ff7f83
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2010-12-01 19:45:39 +0000

    feedback admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -256,7 +256,8 @@ class UsersController < ApplicationController
 	  @user.attributes = params[:user]
 	  @user.build_retailer_application unless @user.retailer_application
 	  @user.build_addresses("billing", "shipping", "home")
-	  @user.retailer_application.attributes = params[:user][:retailer_application]	  
+	  @user.retailer_application.attributes = params[:user][:retailer_application]
+	  @user.tax_exempt_certificate ||= @user.retailer_application.resale_number
 	  if @user.save
 	    @user.retailer_application.save
 	    redirect_to(myaccount_path, :notice => 'Thank you for submitting your application as an Authorized Ellison Retailer. Your request is currently being processed and is pending approval. While we confirm your information, please take a tour of the website. However, please remember that your special pricing can only be accessed once your application has been approved. Thanks for your patience, and we look forward to serving you.')
