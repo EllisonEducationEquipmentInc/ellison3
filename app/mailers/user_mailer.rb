335c7f34d0c19a388a4d694c61dd749191073ce4
jdacio <jdacio@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-01-04 19:52:25 +0000

    order confirmation email layout & style

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,12 @@ class UserMailer < ActionMailer::Base
   
   def order_confirmation(order)
     @order = order
-    mail(:to => order.user.email, :subject => "#{get_domain.capitalize} Order confirmation.")
+    mail(:to => order.user.email, :subject => "#{get_domain.capitalize} Order Confirmation")
   end
   
   def quote_confirmation(quote)
     @quote = quote
-    mail(:to => quote.user.email, :subject => "#{get_domain.capitalize} #{quote_name} confirmation.")
+    mail(:to => quote.user.email, :subject => "#{get_domain.capitalize} #{quote_name} Confirmation")
   end
   
   def email_list(user, name, recipients, wishlist, note)
