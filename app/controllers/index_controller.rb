c8d45de125cb25ad91be2b1f7fb6915658ab595d
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-01-07 20:57:04 +0000

    lp complete

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -47,6 +47,7 @@ class IndexController < ApplicationController
     params[:facets] = @landing_page.search_query
     params[:outlet] = "1" if @landing_page.outlet
     params[:sort] = "start_date_#{current_system}:desc"
+    @title = @landing_page.name
     get_search
     @products = @search.results
     fresh_when(:etag => [current_locale, current_system, @landing_page], :last_modified => @landing_page.updated_at.utc)
