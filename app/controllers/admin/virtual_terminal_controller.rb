d1ec35d5d61d5ec7147e752ca4bc562297174b1b
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-01-28 00:31:36 +0000

    virtual terminal cc

--- app/controllers/admin/virtual_terminal_controller.rb
+++ app/controllers/admin/virtual_terminal_controller.rb
@@ -50,7 +50,7 @@ class Admin::VirtualTerminalController < ApplicationController
     order = Order.new(params[:cch_return])
     @cch = CCH::Cch.new(:action => 'ProcessAttributedReturn', :shipping_charge => order.shipping_amount, :handling_charge => order.handling_amount.present? ? order.handling_amount : 0.0, :total => order.subtotal_amount, :transaction_id => order.tax_transaction)
     if @cch.success?
-      VirtualTransaction.create(:user => current_user, :transaction_type => "cch_return", :result => @cch.total_tax.to_s, :raw_result => @cch.pretty, :transaction_id => @cch.transaction_id, :details => {:order => order.attributes.to_hash})
+      VirtualTransaction.create(:user => current_admin.employee_number, :transaction_type => "cch_return", :result => @cch.total_tax.to_s, :raw_result => @cch.pretty, :transaction_id => @cch.transaction_id, :details => {:order => order.attributes.to_hash})
       render :text => "Total Tax: #{@cch.total_tax} CCH Transaction ID: #{@cch.transaction_id}"
     else
       render :text => @cch.errors #pretty(:request)
@@ -60,6 +60,18 @@ class Admin::VirtualTerminalController < ApplicationController
   end
 	
 	def cc_purchase
-	  
+	  order = Order.new(params[:cc_purchase])	  
+    if params[:purchase_total_amount].to_f > 0
+      process_card(:amount => (params[:purchase_total_amount].to_f * 100).round, :payment => order.payment, :order => params[:purchase_order_id], :capture => params[:purchase_transaction_type] != "authorize", :use_payment_token => false, :system => params[:purchase_system])
+      VirtualTransaction.create(:user => current_admin.employee_number, :transaction_type => params[:purchase_transaction_type], :result => @payment.status, :raw_result => @payment.authorization, :transaction_id => @payment.security_key, :details => {:payment => @payment.attributes.to_hash})
+    end
+    text = ""
+    ["subscriptionid", "cv2_result", "status", "vpstx_id", "security_key", "tx_auth_no", "status_detail", "address_result", "post_code_result", "paid_amount", "authorization"].each do |s|
+      text << "#{s.titleize}: #{@payment.send(s)}<br />"
+    end
+    text << "<br>AX URL Encoded Authorization string: #{CGI::escape(@payment.authorization)}" if @payment.authorization
+    render :text => text
+  rescue Exception => e
+    render :text => e.to_s #+ "\n" + e.backtrace.join("\n")
 	end
 end
