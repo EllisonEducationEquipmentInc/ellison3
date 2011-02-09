d4d057c1bf1bc3bd3d7ece75d1bfaef2da012ca3
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-02-09 23:16:20 +0000

    description

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -145,7 +145,7 @@ class Idea
 	
 	def description(options = {})
 		system = options[:system] || current_system
-		send("description_#{system}") || send("description_er") || send("description_szus")
+		send("description_#{system}") || send("description_er") || send("description_szus") || send("description_eeus")
 	end
 	
 	def description=(d)
