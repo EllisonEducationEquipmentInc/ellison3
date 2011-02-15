aa6d2dc3bf5daa908a52a7a5249eb9c891d542a6
jdacio <jdacio@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-02-15 19:34:09 +0000

    video icon on thumbs refs #730

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -248,7 +248,7 @@ HTML
 	
 	def video_thumbnail(youtube_id)
 	  content_tag :div, :class => "video-thumbnail tooltip_playvideo", :style => "background: url('http://i1.ytimg.com/vi/#{youtube_id}/default.jpg') no-repeat center center;" do
-	    link_to image_tag("/images/ui-icons/set_fugue_custom/24x24/drop-shadow/ell3_film--glow.png"), "http://www.youtube.com/v/#{youtube_id}&amp;feature=youtube_gdata_player&amp;autoplay=1&amp;fs=1", :class => "fancyvideo", :id => youtube_id
+	    link_to image_tag("/images/ui-buttons/play-video_on.png"), "http://www.youtube.com/v/#{youtube_id}&amp;feature=youtube_gdata_player&amp;autoplay=1&amp;fs=1", :class => "fancyvideo", :id => youtube_id
 	  end
 	end
 	
