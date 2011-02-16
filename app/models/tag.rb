c0e95297deff25d37c2b56014ef9bcbcf5a72b18
jdacio <jdacio@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-02-16 19:45:23 +0000

    catalog facet filter tweaks 3) & 4) refs #751

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -9,7 +9,7 @@ class Tag
   
   attr_accessor :embed_campaign
   
-  TYPES = ["artist", "category", "curriculum", "designer", "machine_compatibility", "material_compatibility", "product_family", "product_line", "special", "subcategory", "subcurriculum", "subtheme", "theme", "release_date", "size", "grade_level"]
+  TYPES = ["category", "subcategory", "product_line", "product_family", "theme", "subtheme", "curriculum", "subcurriculum", "designer", "artist", "grade_level", "release_date", "machine_compatibility", "material_compatibility", "size", "special"]
   HIDDEN_TYPES = ["exclusive", "calendar_event"]
   
   references_and_referenced_in_many :products, :index => true
