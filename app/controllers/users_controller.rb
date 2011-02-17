5b68350552c505876bc8f104e32c1642596341cc
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-02-17 19:40:44 +0000

    ssl allowed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
   before_filter :trackable
   include Devise::Controllers::InternalHelpers
   
-  ssl_exceptions :signin_signup, :checkout_requested, :quote_requested
-  ssl_allowed :signin_signup, :checkout_requested, :quote_requested
+  ssl_exceptions :signin_signup, :checkout_requested, :quote_requested, :add_to_list, :save_for_later, :list, :get_lists
+  ssl_allowed :signin_signup, :checkout_requested, :quote_requested, :add_to_list, :save_for_later, :list, :get_lists
 
   verify :xhr => true, :only => [:checkout_requested, :quote_requested, :billing, :shipping, :edit_address, :orders, :mylists, :quotes, :materials, :update_list, :create_list, :delete_list, :save_for_later, :add_to_list, :list_set_to_default, :remove_from_list, :move_to_list, :email_list, :view_retailer_application], :redirect_to => {:action => :myaccount}
   verify :post => true, :only => [:create_retailer_application, :order_material]
