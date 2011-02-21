0bad11d169ee7781910e3a06ce4de1a586d88232
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-02-21 16:45:04 +0000

    Auto complete case insencitive fixes #739 fixes #755

--- app/middlewares/solr_terms.rb
+++ app/middlewares/solr_terms.rb
@@ -12,20 +12,20 @@ class SolrTerms
     solr_path = "http://#{Sunspot::Rails.configuration.hostname}:#{Sunspot::Rails.configuration.port}#{Sunspot::Rails.configuration.path}"
     if env["PATH_INFO"] =~ /^\/solr_terms\/(.+)$/
       term = $1
-      [200, {"Content-Type" => 'text/plain; charset=utf-8'}, [Net::HTTP.get(URI.parse("#{solr_path}/terms?terms.fl=spell&terms.prefix=#{URI.escape(term)}&terms.mincount=5&terms.sort=index&indent=true&wt=json&omitHeader=true&json.nl=arrarr"))]]
+      [200, {"Content-Type" => 'text/plain; charset=utf-8'}, [Net::HTTP.get(URI.parse("#{solr_path}/terms?terms.fl=spell&terms.prefix=#{URI.escape(term.downcase)}&terms.mincount=5&terms.sort=index&indent=true&wt=json&omitHeader=true&json.nl=arrarr"))]]
     elsif env["REQUEST_URI"] =~ /^\/products_autocomplete\?term=(.+)$/
       term = $1
-      solr_response = Net::HTTP.get(URI.parse("#{solr_path}/select?fq=type%3AProduct&fq=active_b%3Atrue&q=#{term.strip.gsub(/\+$/, '')}*&fl=id+item_num_ss+stored_name_ss+msrp_usd_fs+msrp_gbp_fs+msrp_eur_fs&qf=item_num_ss+stored_name_ss&start=0&rows=10&wt=ruby&omitHeader=true&json.nl=arrarr"))
+      solr_response = Net::HTTP.get(URI.parse("#{solr_path}/select?fq=type%3AProduct&fq=active_b%3Atrue&q=#{term.downcase.strip.gsub(/\+$/, '')}*&fl=id+item_num_ss+stored_name_ss+msrp_usd_fs+msrp_gbp_fs+msrp_eur_fs&qf=item_num_ss+stored_name_ss&start=0&rows=10&wt=ruby&omitHeader=true&json.nl=arrarr"))
       hash = eval(solr_response)
-      [200, {"Content-Type" => 'text/plain; charset=utf-8'}, hash["response"]["docs"].inject([]) {|arr, e| arr << {"id" => e['id'].gsub("Product ", ''), "label" => "#{e['item_num_ss']} - #{e['stored_name_ss']}", "value" => e['item_num_ss']}}.to_json]
+      [200, {"Content-Type" => 'text/plain; charset=utf-8'}, hash["response"]["docs"].inject([]) {|arr, e| arr << {"id" => e['id'].gsub("Product ", ''), "label" => "#{e['item_num_ss']} - #{e['stored_name_ss']}", "value" => e['item_num_ss'], "msrp_usd" => e['msrp_usd_fs'], "msrp_gbp" => e['msrp_gbp_fs'], "msrp_eur" => e['msrp_eur_fs']}}.to_json]
     elsif env["REQUEST_URI"] =~ /^\/ideas_autocomplete\?term=(.+)$/
       term = $1
-      solr_response = Net::HTTP.get(URI.parse("#{solr_path}/select?fq=type%3AIdea&fq=active_b%3Atrue&q=#{term.strip.gsub(/\+$/, '')}*&fl=id+idea_num_ss+stored_name_ss&qf=idea_num_ss+stored_name_ss&start=0&rows=10&wt=ruby&omitHeader=true&json.nl=arrarr"))
+      solr_response = Net::HTTP.get(URI.parse("#{solr_path}/select?fq=type%3AIdea&fq=active_b%3Atrue&q=#{term.downcase.strip.gsub(/\+$/, '')}*&fl=id+idea_num_ss+stored_name_ss&qf=idea_num_ss+stored_name_ss&start=0&rows=10&wt=ruby&omitHeader=true&json.nl=arrarr"))
       hash = eval(solr_response)
       [200, {"Content-Type" => 'text/plain; charset=utf-8'}, hash["response"]["docs"].inject([]) {|arr, e| arr << {"id" => e['id'].gsub("Idea ", ''), "label" => "#{e['idea_num_ss']} - #{e['stored_name_ss']}", "value" => e['idea_num_ss']}}.to_json]
     elsif env["REQUEST_URI"] =~ /^\/tags_autocomplete\?term=(.+)$/
       term = $1
-      solr_response = Net::HTTP.get(URI.parse("#{solr_path}/select?fq=type%3ATag&fq=active_b%3Atrue&q=#{term.strip.gsub(/\+$/, '')}*&fl=id+stored_name_ss+tag_type_ss&qf=stored_name_ss&start=0&rows=10&wt=ruby&omitHeader=true&json.nl=arrarr"))
+      solr_response = Net::HTTP.get(URI.parse("#{solr_path}/select?fq=type%3ATag&fq=active_b%3Atrue&q=#{term.downcase.strip.gsub(/\+$/, '')}*&fl=id+stored_name_ss+tag_type_ss&qf=stored_name_ss&start=0&rows=10&wt=ruby&omitHeader=true&json.nl=arrarr"))
       hash = eval(solr_response)
       [200, {"Content-Type" => 'text/plain; charset=utf-8'}, hash["response"]["docs"].inject([]) {|arr, e| arr << {"id" => e['id'].gsub("Tag ", ''), "label" => "#{e['stored_name_ss']} (#{e['tag_type_ss']})", "value" => e['stored_name_ss']}}.to_json]
     else
