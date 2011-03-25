8b1a8b01770e32bf72f4a47684cd7c465cd5b956
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-03-25 21:30:15 +0000

    admin/quotes issues/enhancements 3) 4) refs #878

--- app/controllers/admin/quotes_controller.rb
+++ app/controllers/admin/quotes_controller.rb
@@ -21,6 +21,7 @@ class Admin::QuotesController < ApplicationController
 	  end
 	  criteria = criteria.where(:status => params[:status]) unless params[:status].blank?
 	  unless params[:q].blank?
+	    redirect_to(admin_quote_path(:id => params[:q])) if params[:q].valid_bson_object_id?
 	    regexp = Regexp.new(params[:q], "i")
   	  criteria = criteria.any_of({:name => regexp}, { 'address.first_name' => regexp}, { 'address.last_name' => regexp }, { 'address.city' => regexp }, { 'address.address' => regexp })
 	  end
