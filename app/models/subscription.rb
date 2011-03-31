a0852e607c05cd0abb2f2ccc2420cabe78017d14
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-03-31 23:40:37 +0000

    validates_presence_of

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -14,7 +14,7 @@ class Subscription
   field :unsubscribe, :type => Boolean, :default => false
   field :verification_sent, :type => DateTime
   
-  validates_presence_of :email
+  validates_presence_of :email, :list_name
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates_uniqueness_of :email, :scope => :list, :message => "address has already been subscribed."
   
