765e781a0cda20303a0f8585f1e3a5f2b9a84c9c
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-04-01 18:59:29 +0000

    email

--- app/controllers/admin/virtual_terminal_controller.rb
+++ app/controllers/admin/virtual_terminal_controller.rb
@@ -22,7 +22,7 @@ class Admin::VirtualTerminalController < ApplicationController
     if calculate_tax?(@order.address.state) || @order.address.country == "Canada"
       @cch = CCH::Cch.new(:action => 'calculate', :customer => @order.address, :shipping_charge => @order.shipping_amount, :handling_charge => @order.handling_amount.present? ? @order.handling_amount : 0.0, :total => @order.subtotal_amount, :exempt => @order.tax_exempt, :tax_exempt_certificate => @order.tax_exempt_number)
       if @cch.success?
-        VirtualTransaction.create(:user => current_admin.employee_number, :transaction_type => "cch_calculate", :result => @cch.total_tax.to_s, :raw_result => @cch.pretty, :transaction_id => @cch.transaction_id, :details => {:order => @order.attributes.to_hash})
+        VirtualTransaction.create(:user => current_admin.email, :transaction_type => "cch_calculate", :result => @cch.total_tax.to_s, :raw_result => @cch.pretty, :transaction_id => @cch.transaction_id, :details => {:order => @order.attributes.to_hash})
         render :text => "Total Tax: #{@cch.total_tax} CCH Transaction ID: #{@cch.transaction_id}"
       else
         render :text => @cch.errors
@@ -37,7 +37,7 @@ class Admin::VirtualTerminalController < ApplicationController
 	def cch_commit
     @cch = CCH::Cch.new(:action => 'commit', :transaction_id => params[:cch_commit_tax_transaction_id])
     if @cch.success?
-      VirtualTransaction.create(:user => current_admin.employee_number, :transaction_type => "cch_commit", :result => "Committed", :raw_result => @cch.pretty, :transaction_id => params[:cch_commit_tax_transaction_id], :details => {:transaction_id => params[:cch_commit_tax_transaction_id]})
+      VirtualTransaction.create(:user => current_admin.email, :transaction_type => "cch_commit", :result => "Committed", :raw_result => @cch.pretty, :transaction_id => params[:cch_commit_tax_transaction_id], :details => {:transaction_id => params[:cch_commit_tax_transaction_id]})
       render :text => "#{params[:cch_commit_tax_transaction_id]} committed"
     else
       render :text => @cch.errors
@@ -50,7 +50,7 @@ class Admin::VirtualTerminalController < ApplicationController
     order = Order.new(params[:cch_return])
     @cch = CCH::Cch.new(:action => 'ProcessAttributedReturn', :shipping_charge => order.shipping_amount, :handling_charge => order.handling_amount.present? ? order.handling_amount : 0.0, :total => order.subtotal_amount, :transaction_id => order.tax_transaction)
     if @cch.success?
-      VirtualTransaction.create(:user => current_admin.employee_number, :transaction_type => "cch_return", :result => @cch.total_tax.to_s, :raw_result => @cch.pretty, :transaction_id => @cch.transaction_id, :details => {:order => order.attributes.to_hash})
+      VirtualTransaction.create(:user => current_admin.email, :transaction_type => "cch_return", :result => @cch.total_tax.to_s, :raw_result => @cch.pretty, :transaction_id => @cch.transaction_id, :details => {:order => order.attributes.to_hash})
       render :text => "Total Tax: #{@cch.total_tax} CCH Transaction ID: #{@cch.transaction_id}"
     else
       render :text => @cch.errors #pretty(:request)
@@ -63,7 +63,7 @@ class Admin::VirtualTerminalController < ApplicationController
 	  order = Order.new(params[:cc_purchase])	  
     if params[:purchase_total_amount].to_f > 0
       process_card(:amount => (params[:purchase_total_amount].to_f * 100).round, :payment => order.payment, :order => params[:purchase_order_id], :capture => params[:purchase_transaction_type] != "authorize", :use_payment_token => false, :system => params[:purchase_system])
-      VirtualTransaction.create(:user => current_admin.employee_number, :transaction_type => params[:purchase_transaction_type], :result => @payment.status, :raw_result => @payment.authorization, :transaction_id => @payment.security_key, :details => {:payment => @payment.attributes.to_hash})
+      VirtualTransaction.create(:user => current_admin.email, :transaction_type => params[:purchase_transaction_type], :result => @payment.status, :raw_result => @payment.authorization, :transaction_id => @payment.security_key, :details => {:payment => @payment.attributes.to_hash})
     end
     text = ""
     ["subscriptionid", "cv2_result", "status", "vpstx_id", "security_key", "tx_auth_no", "status_detail", "address_result", "post_code_result", "paid_amount", "authorization"].each do |s|
@@ -79,7 +79,7 @@ class Admin::VirtualTerminalController < ApplicationController
     get_gateway params[:cc_capture_system]
     @net_response = @gateway.capture(params[:cc_capture_total_amount].to_f * 100, params[:authorization])
     if @net_response.success?
-      VirtualTransaction.create(:user => current_admin.employee_number, :transaction_type => "cc_capture", :result => @net_response.params.inspect, :raw_result => @net_response.inspect, :transaction_id => @net_response.authorization, :details => {:amount_to_charge => params[:cc_capture_total_amount], :authorization => params[:authorization]})
+      VirtualTransaction.create(:user => current_admin.email, :transaction_type => "cc_capture", :result => @net_response.params.inspect, :raw_result => @net_response.inspect, :transaction_id => @net_response.authorization, :details => {:amount_to_charge => params[:cc_capture_total_amount], :authorization => params[:authorization]})
     else
       raise @net_response.message
     end
@@ -94,7 +94,7 @@ class Admin::VirtualTerminalController < ApplicationController
 			@net_response = @gateway.credit(params[:amount_to_refund].to_f * 100, params[:refund_authorization])
 		end
     if @net_response.success?
-      VirtualTransaction.create(:user => current_admin.employee_number, :transaction_type => "cc_refund", :result => @net_response.params.inspect, :raw_result => @net_response.inspect, :transaction_id => @net_response.authorization, :details => {:amount_to_refund => params[:amount_to_refund], :authorization => params[:refund_authorization]})
+      VirtualTransaction.create(:user => current_admin.email, :transaction_type => "cc_refund", :result => @net_response.params.inspect, :raw_result => @net_response.inspect, :transaction_id => @net_response.authorization, :details => {:amount_to_refund => params[:amount_to_refund], :authorization => params[:refund_authorization]})
     else
       raise @net_response.message
     end
