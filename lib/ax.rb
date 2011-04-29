6a8a0bf0134f52277fb46d2bdb1dd1e9ffbd0b1c
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-04-29 17:47:49 +0000

    Delete education US subscriptions once order is shipped fixes #871

--- lib/ax.rb
+++ lib/ax.rb
@@ -160,7 +160,7 @@ module Ax
 	            dborder.update_attributes!(:status => order_status, :tracking_number => tracking_number, :tracking_url => tracking_url, :carrier_description => carrier_description)							
   						if order_status == "Shipped"
   						  dborder.send_shipping_confirmation 
-  						  dborder.delay.delete_billing_subscription_id if dborder.system != "erus" && dborder.payment && dborder.payment.subscriptionid.present?
+  						  dborder.delay.delete_billing_subscription_id if dborder.system == "eeus" && dborder.payment && dborder.payment.subscriptionid.present?
   						  #commit_tax(dborder) if !dborder.tax_committed
   						end
 	          end
