6d2fc5e0428b7a2ef884a6390c613c94955aff73
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-04-29 18:21:06 +0000

    Able to change/reset password even if password and password confirmation field does not match fixes #991

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,6 +51,7 @@ class User
 	validates_numericality_of :order_minimum, :first_order_minimum, :allow_nil => true, :only_integer => true
 	validates_format_of :password,	:if => :password_required?, :with => /((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15})/i, :message => "must contain at least one letter and one digit, length must be between 8 and 15 characters"
 	validates_length_of :password, :within => 8..15, :if => :password_required?
+	validates_confirmation_of :password, :if => :password_required?
 	
 	attr_accessible :name, :company, :email, :password, :password_confirmation, :addresses_attributes, :institution, :retailer_application_attributes
 	
