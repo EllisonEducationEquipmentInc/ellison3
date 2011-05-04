fe5f4bd402d1e7dc9db0f95fa9b909d0df92907a
mhall <mhall@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-05-04 18:10:33 +0000

    Sitewide link updates

--- app/models/top_navigation.rb
+++ app/models/top_navigation.rb
@@ -30,11 +30,12 @@ private
   
   def list_szuk
     [
-      Navigation.new("Sizzix Products", "/catalog"), 
-      Navigation.new("Craft Projects", "/lp/categories?ideas=1"), 
+      Navigation.new("Products", "/catalog"), 
+      Navigation.new("Projects", "/lp/categories?ideas=1"), 
       Navigation.new("Promotions", "/campaigns"),
       Navigation.new("Featured", "#"),
-      Navigation.new("Community", "#")
+      Navigation.new("Community", "#"),
+      Navigation.new("Support", "#")
     ]
   end
   
@@ -44,7 +45,8 @@ private
       Navigation.new("Products", "/catalog"), 
       Navigation.new("Electronic Cutting", "/shop/eclips"),
       Navigation.new("Specials", "/campaigns"),
-      Navigation.new("Community", "#")
+      Navigation.new("Community", "#"),
+      Navigation.new("Support", "#")
     ]
   end
   
@@ -52,9 +54,9 @@ private
     [
       Navigation.new("Lessons", "/lp/categories?ideas=1"), 
       Navigation.new("Products", "/catalog"), 
-      Navigation.new("Electronic Cutting", "/shop/eclips"),
       Navigation.new("Specials", "/campaigns"),
-      Navigation.new("Community", "#")
+      Navigation.new("Community", "#"),
+      Navigation.new("Support", "#")
     ]
   end
   
@@ -63,7 +65,9 @@ private
       Navigation.new("Craft Products", "/shop/sizzixcrafting"), 
       Navigation.new("Craft Projects", "/shop/sizzixcrafting"), 
       Navigation.new("Education Products", "/shop/ellisoneducation"),
-      Navigation.new("Education Projects", "/shop/ellisoneducation")
+      Navigation.new("Education Projects", "/shop/ellisoneducation"),
+      Navigation.new("Community", "#"),
+      Navigation.new("Support", "#")      
     ]
   end
   
