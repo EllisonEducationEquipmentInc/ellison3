c86f0397356aabf61ebc654bfe87ddd0833f1496
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-05-11 23:17:56 +0000

    compress css

--- config/compass.rb
+++ config/compass.rb
@@ -7,5 +7,6 @@ http_path = "/"
 css_dir = "public/stylesheets/compiled"
 sass_dir = "app/stylesheets"
 environment = Compass::AppIntegration::Rails.env
+output_style = (environment == :production) ? :compressed : :expanded
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
