d28af9b11e64d00c3560bd01489219b205ed418b
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-05-23 23:42:47 +0000

    options[:per_page]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -377,8 +377,8 @@ private
           end
         end
       end
-      Rails.logger.info "@per_page || per_page #{@per_page || per_page}"
-      query.paginate(:page => params[:page] || 1, :per_page => @per_page || per_page)
+      Rails.logger.info "@per_page || per_page #{@per_page || options[:per_page] || per_page}"
+      query.paginate(:page => params[:page] || 1, :per_page => @per_page || options[:per_page] || per_page)
       query.order_by(*default_sort(klass).split(":")) unless default_sort(klass).blank? || klass == Idea && ['quantity_sold', 'price', 'orderable', 'outlet_since'].any? {|e| default_sort(klass).include? e}
     end
   end
