e99be150e453ca737e861b76512a52c6177fe8e6
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-05-31 18:03:47 +0000

    zoom image for idea

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -292,7 +292,7 @@ private
 
 	def get_image(version)
 		if image?
-			image_url(version)
+      version.to_s == 'zoom' ? image_url : image_url(version) 
 		else
 		  return image.default_url_edu(version) if (is_ee? || is_er?) && FileTest.exists?("#{Rails.root}/public/#{image.default_url_edu(version)}")
 			FileTest.exists?("#{Rails.root}/public/#{image.default_url(version)}") ? image.default_url(version) : "/images/ideas/#{version}/noimage.jpg"
