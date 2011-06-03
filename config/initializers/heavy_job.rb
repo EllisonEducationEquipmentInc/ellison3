8b6a13a3a6a41a986d1c2dcb165d18739cc6c897
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-06-03 16:11:22 +0000

    upgrade newrelic and temporarily switch to free 14 day pro trial

--- config/initializers/heavy_job.rb
+++ config/initializers/heavy_job.rb
@@ -1,8 +1,9 @@
-# use this class to perform heavy operations in the background by DJ (ddelayed_job)
+# use this class to perform heavy operations in the background by DJ (delayed_job)
 # example: @tag.products.nullify caused passenger to grow memory over the limit.
 # with "HeavyJob":
 #
 #   Delayed::Job.enqueue HeavyJob.new @tag, :products, :nullify, :save, :validate => false
+#
 HeavyJob = Struct.new(:obj, :collection, :method_to_perform, :callback, :callback_args) do
   def perform
     obj.send(collection).send(method_to_perform)
