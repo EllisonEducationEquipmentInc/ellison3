04dfe08d0fa68f17ff43da98bfd15c3c1d859543
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-07-12 23:35:40 +0000

    subscription case insensitive

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -16,7 +16,7 @@ class Subscription
   
   validates_presence_of :email, :list_name
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  validates_uniqueness_of :email, :scope => :list, :message => "address has already been subscribed."
+  validates_uniqueness_of :email, :scope => :list, :message => "address has already been subscribed.", :case_sensitive => false,
   
   scope :confirmed, :where => { :confirmed => true }
  
