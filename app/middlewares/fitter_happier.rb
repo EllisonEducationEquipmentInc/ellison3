7849a617e50e928142ef9786d9dedcf633ba2da2
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-07-21 18:20:39 +0000

    site_solr_and_database_check

--- app/middlewares/fitter_happier.rb
+++ app/middlewares/fitter_happier.rb
@@ -11,7 +11,14 @@ class FitterHappier
       when "/site_check"
         [200, {"Content-Type" => 'text/html; charset=utf-8'}, ["FitterHappier Site Check Passed @ #{time}\n"]]
       when "/site_and_database_check"
-        [200, {"Content-Type" => 'text/html; charset=utf-8'}, ["FitterHappier Site and Database Check Passed @ #{time}\nDatabase: #{Digest::SHA1.hexdigest(Product.count)}\n"]]
+        begin
+          Product.count
+          [200, {"Content-Type" => 'text/html; charset=utf-8'}, ["FitterHappier Site and Database Check Passed @ #{time}\n"]]
+        rescue Mongo::ConnectionFailure => e
+          [500, {"Content-Type" => 'text/html; charset=utf-8'}, ["!!! ALERT !!! MONGODB IS DOWN!!! @ #{time}\n"]]
+        rescue Exception => e
+          [500, {"Content-Type" => 'text/html; charset=utf-8'}, ["!!! ALERT !!! INTERNAL SERVER ERROR!!! @ #{time}\n#{e}"]]
+        end
       when "/site_solr_and_database_check"
         begin
           resp = Net::HTTP.get(URI.parse("http://#{Sunspot::Rails.configuration.hostname}:#{Sunspot::Rails.configuration.port}#{Sunspot::Rails.configuration.path}/terms?terms.fl=spell&terms.prefix=sizz&terms.mincount=5&terms.sort=index&indent=true&wt=json&omitHeader=true&json.nl=arrarr"))
