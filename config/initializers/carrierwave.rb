a42ff62431b89087e07e9666c5b6e9377f92a416
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-07-26 18:52:48 +0000

    Fails to view uploaded images on carbon and qa sites after mongodb replica configuration fixes #1171

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,7 @@ CarrierWave.configure do |config|
 	config.validate_integrity = true
   config.grid_fs_database = Mongoid.database.name
   config.grid_fs_host = Mongoid.database.connection.primary_pool.host 
+  config.grid_fs_port = Mongoid.database.connection.primary_pool.port
   config.storage = :grid_fs
   config.grid_fs_access_url = "/grid"
 end
