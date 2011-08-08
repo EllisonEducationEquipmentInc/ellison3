c5525571949626bef9dad35bfd7a209f53d9d081
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-08-08 16:42:35 +0000

    order summary report discrepencies fixes #1183

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -23,15 +23,15 @@ class Admin::ReportsController < ApplicationController
 	
 	def order_summary
 	  @original_locale = current_locale
-    @order_summary = Order.summary :start_date => Time.parse(params[:start_date]), :end_date => Time.parse(params[:end_date]), :system => params[:order_system]
-    @order_statuses = Order.status_summary :start_date => Time.parse(params[:start_date]), :end_date => Time.parse(params[:end_date]), :system => params[:order_system]
+    @order_summary = Order.summary :start_date => Time.zone.parse(params[:start_date]), :end_date => Time.zone.parse(params[:end_date]), :system => params[:order_system]
+    @order_statuses = Order.status_summary :start_date => Time.zone.parse(params[:start_date]), :end_date => Time.zone.parse(params[:end_date]), :system => params[:order_system]
 	end
 	
 	def wishlists_report
-	  criteria = List.listable.where(:owns.ne => true, :created_at.gt => Time.parse(params[:start_date]), :created_at.lt => Time.parse(params[:end_date]), :system => params[:order_system])
+	  criteria = List.listable.where(:owns.ne => true, :created_at.gt => Time.zone.parse(params[:start_date]), :created_at.lt => Time.zone.parse(params[:end_date]), :system => params[:order_system])
 		#@total_wishlists = criteria.count
 		@wishlist_users = criteria.distinct(:user_id).count
-		@items_per_lists = List.items_per_list :system => params[:order_system], :query => {:created_at => {"$gt" => Time.parse(params[:start_date]), "$lt" => Time.parse(params[:end_date])}, :active => true, :save_for_later => {"$ne"=>true}, :owns => {"$ne"=>true}, :system => params[:order_system]}
+		@items_per_lists = List.items_per_list :system => params[:order_system], :query => {:created_at => {"$gt" => Time.zone.parse(params[:start_date]), "$lt" => Time.zone.parse(params[:end_date])}, :active => true, :save_for_later => {"$ne"=>true}, :owns => {"$ne"=>true}, :system => params[:order_system]}
 	end
 	
 	def get_status
@@ -43,7 +43,7 @@ class Admin::ReportsController < ApplicationController
 	  if params[:campaign].blank?
 	    render :js => "alert('select campaign from the dropdown')"
 	  else
-	    @report = Report.create :report_options => {:name => params[:campaign].parameterize}, :start_date => Time.parse(params[:start_date]), :end_date => Time.parse(params[:end_date]), :system => current_system
+	    @report = Report.create :report_options => {:name => params[:campaign].parameterize}, :start_date => Time.zone.parse(params[:start_date]), :end_date => Time.zone.parse(params[:end_date]), :system => current_system
   	  @report.delay.campaign_coupon_usage(params[:campaign])
   	  render :process
 	  end
