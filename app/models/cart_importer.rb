7a07385650e6fcfcb17d70e57972d185a3dc3969
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-09-28 17:47:46 +0000

    enet ticket no.1923 - Ability to expor items to cart refs #1212

--- app/models/cart_importer.rb
+++ app/models/cart_importer.rb
@@ -23,16 +23,21 @@ class CartImporter
 	def populate_cart
 	  n = 0
 	  CSV.foreach(self.file_name, :headers => true, :row_sep => :auto, :skip_blanks => true, :quote_char => '"') do |row|
-      product = Product.available.where(:item_num => row['item_num']).first
-      unless product
+      product = Product.available.where(:item_num => row['item_num'].upcase).first
+      unless product.present? && product.orderable? && product.can_be_added_to_cart?
         update_attribute :import_errors, self.import_errors << row['item_num']
         next
       end
   	  qty = row['item_num'].blank? ? is_er? ? product.minimum_quantity : 1 : row['qty'].to_i
   	  qty = product.minimum_quantity if is_er? && qty < product.minimum_quantity
-  	  self.cart.cart_items << CartItem.new(:name => product.name, :item_num => product.item_num, :sale_price => product.outlet? ? product.price : product.sale_price, :msrp => product.msrp_or_wholesale_price, :price => product.price, 
-			  :quantity => qty, :currency => current_currency, :small_image => product.small_image, :added_at => Time.now, :product => product, :weight => product.virtual_weight, :actual_weight => product.weight, :retailer_price => product.retailer_price,
-			  :tax_exempt => product.tax_exempt, :handling_price => product.handling_price, :pre_order => product.pre_order?, :out_of_stock => product.out_of_stock?, :minimum_quantity => product.minimum_quantity, :campaign_name => product.campaign_name, :outlet => product.outlet?)
+  	  if item = self.cart.cart_items.find_item(product.item_num)
+				item.quantity += qty
+				self.cart.save
+			else
+    	  self.cart.cart_items << CartItem.new(:name => product.name, :item_num => product.item_num, :sale_price => product.outlet? ? product.price : product.sale_price, :msrp => product.msrp_or_wholesale_price, :price => product.price, 
+  			  :quantity => qty, :currency => current_currency, :small_image => product.small_image, :added_at => Time.now, :product => product, :weight => product.virtual_weight, :actual_weight => product.weight, :retailer_price => product.retailer_price,
+  			  :tax_exempt => product.tax_exempt, :handling_price => product.handling_price, :pre_order => product.pre_order?, :out_of_stock => product.out_of_stock?, :minimum_quantity => product.minimum_quantity, :campaign_name => product.campaign_name, :outlet => product.outlet?)
+  		end
     	n += 1
       percentage!(n)
     end
