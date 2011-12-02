10adedef76aa5bae3cbea0748c56faafe0a4327c
mhall <mhall@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2011-12-02 22:43:19 +0000

    store controller tweak

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -17,7 +17,7 @@ class Admin::StoresController < ApplicationController
 	  criteria = criteria.where(:product_line.in => [params[:product_line]]) unless params[:product_line].blank?
 	  unless params[:q].blank?
 	    regexp = Regexp.new(params[:q], "i")
-  	  criteria = criteria.any_of({ :name => regexp}, { :contact_person => regexp }, {:store_number => regexp})
+  	  criteria = criteria.any_of({ :name => regexp}, { :contact_person => regexp }, {:store_number => regexp}, {:zip_code =>regexp})
 	  end
 		@stores = criteria.order_by(sort_column => sort_direction).paginate :page => params[:page], :per_page => 50
 	end
