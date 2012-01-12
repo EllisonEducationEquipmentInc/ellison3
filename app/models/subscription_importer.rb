733d8a1ccfb5c93179757a16238cfec1fcae1701
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2012-01-12 01:20:19 +0000

    Web and Lyris Integration subscription enhancements / users requests refs #1215

--- app/models/subscription_importer.rb
+++ app/models/subscription_importer.rb
@@ -22,6 +22,7 @@ class SubscriptionImporter
     n = 0
     CSV.foreach(self.file_name, :headers => true, :row_sep => :auto, :skip_blanks => true, :quote_char => '"') do |row|
       set_current_system list_to_system(row['list'])
+      self.system = current_system
       get_list_and_segments
       @subscription = Subscription.first(:conditions => {:email => row['email'].downcase, :list => subscription_list}) || Subscription.new 
       @subscription.email = row['email'].downcase
@@ -34,10 +35,10 @@ class SubscriptionImporter
       @subscription.segments.uniq!
       if @subscription.save
         Rails.logger.info "===== #{@subscription.valid?} #{@subscription.email} #{@subscription.list} ===="
-	Rails.logger.info "sending id: #{@subscription.segments.map {|e| {:name => e.to_sym, :value => 1}} << {:name => :subscription_id, :value => @subscription.id.to_s}}"
-        Lyris.new :create_single_member, :email_address => @subscription.email, :list_name => @subscription.list, :full_name => @subscription.name
-        Lyris.new :update_member_status, :simple_member_struct_in => {:email_address => @subscription.email, :list_name => @subscription.list}, :member_status => 'normal'
-        Lyris.new :update_member_demographics, :simple_member_struct_in => {:email_address => @subscription.email, :list_name => @subscription.list}, :demographics_array => @subscription.segments.map {|e| {:name => e.to_sym, :value => 1}} << {:name => :subscription_id, :value => @subscription.id.to_s}
+      	Rails.logger.info "sending id: #{@subscription.segments.map {|e| {:name => e.to_sym, :value => 1}} << {:name => :subscription_id, :value => @subscription.id.to_s}}"
+        Lyris.delay.new :create_single_member, :email_address => @subscription.email, :list_name => @subscription.list, :full_name => @subscription.name
+        Lyris.delay.new :update_member_status, :simple_member_struct_in => {:email_address => @subscription.email, :list_name => @subscription.list}, :member_status => 'normal'
+        Lyris.delay.new :update_member_demographics, :simple_member_struct_in => {:email_address => @subscription.email, :list_name => @subscription.list}, :demographics_array => @subscription.segments.map {|e| {:name => e.to_sym, :value => 1}} << {:name => :subscription_id, :value => @subscription.id.to_s}
       else
         update_attribute :import_errors, self.import_errors << row['email']
         Rails.logger.info "!!! subscription record is invalid. #{@subscription.errors}"
