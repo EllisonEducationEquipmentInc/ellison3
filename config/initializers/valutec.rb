28737461ca189c1657b422daeea2f89df14cacba
mronai <mronai@de9111b1-5ba2-4fbf-9abd-e594752d6401>
2012-06-08 00:27:00 +0000

    html in error msg

--- config/initializers/valutec.rb
+++ config/initializers/valutec.rb
@@ -79,10 +79,10 @@ class Valutec
       {
         "CARD NOT ACTIVE" => "The Gift Card number you entered is not active. Please contact Customer Service.",
         "CARD NOT FOUND" => "The Gift Card number you entered is not valid. Please check your number and try again.",
-        "CARD DEACTIVATED" => "The Gift Card number you entered is not active. Please contact Customer Service.",
+        "CARD DEACTIVATED" => "The Gift Card number you entered is not active. Please <a href='/contact'>contact</a> Customer Service.",
         "CANNOT ACCEPT CARD" => "The Pin number you entered is not valid. Please check the number and try again."
       }[results[:error_msg]] || results[:error_msg]
-    end
+    end.html_safe
   end
 
 end
