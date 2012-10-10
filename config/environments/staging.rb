783928f2ef7eaf222bb4569a04717f400adc1271
Justin S. Leitgeb <justin@stackbuilders.com>
2012-10-10 17:18:38 -0400

    [36868349] Turn on serving of static assets in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -32,7 +32,7 @@ Ellison3::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -42,24 +42,24 @@ Ellison3::Application.configure do
 	config.action_mailer.default_url_options = { :host => 'ellison.com' }
 	config.action_mailer.delivery_method = :smtp
 	ey_smtp_settings = {
-    :domain             => "sizzix.com", 
+    :domain             => "sizzix.com",
     :address            => "smtp",
     :port               => 25,
     :perform_deliveries => true
   }
-  #config.action_mailer.smtp_settings = ey_smtp_settings #{:address => "mail.ellison.com", :port => 25} 
-  config.action_mailer.smtp_settings = {:address => "mail.ellison.com", :port => 25} 
+  #config.action_mailer.smtp_settings = ey_smtp_settings #{:address => "mail.ellison.com", :port => 25}
+  config.action_mailer.smtp_settings = {:address => "mail.ellison.com", :port => 25}
 	config.action_mailer.perform_deliveries = true
 	config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   config.threadsafe!
-  
+
   config.dependency_loading = true if $rails_rake_task
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
-  
+
   config.active_support.deprecation = :notify
 end
