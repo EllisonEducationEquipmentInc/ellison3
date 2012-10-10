051cf720f3a464689612ef2993255759715dd69e
Madhavi Barla <mbarla@test.com>
2012-10-10 14:52:15 -0700

    deleting rvm load path on deploy.rb as not needed now

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,3 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_ruby_string, 'ruby-1.9.3-p194@Ellison3'
 
