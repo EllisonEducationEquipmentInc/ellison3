710abed17d4bbf40b1bd3e8eb7cd1d57a9ccc4ef
Justin S. Leitgeb <justin@stackbuilders.com>
2012-10-17 14:09:21 -0400

    [37953959] Add internal deploy target and remove production target
      * Currently Ellison has a separate file for production deploy, which is weird
      * This commit adds an 'internal' target which tries to mimic the internal deploy
        with new rules
      * Production deploy should remain unchanged for the moment, since Ellison is using
        a deploy file out of the repository for this anyway. That should be fixed in the
        future, and production deploy should use the same process as staging/internal.

--- config/deploy/production.rb
+++
@@ -1,8 +0,0 @@
-server 'qa.ellison.stackbuilders.com', :app, :web, :db, :utility, :primary => true
-
-set :user, 'ellison'
-set :rails_env, 'production'
-set :branch, 'production'
-set :deploy_to, "/data/ellison3_production"
-
-set :deploy_to, "/var/projects/ellison/qa"
