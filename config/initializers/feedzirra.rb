12d48bf06ea3af8c44d8d849e8e142b820925d1b
Madhavi Barla <mbarla@test.com>
2012-10-17 21:10:51 -0700

    ruby upgrade blogs fix trial clean up

--- config/initializers/feedzirra.rb
+++ config/initializers/feedzirra.rb
@@ -2,7 +2,7 @@ module Feedzirra
   
   module Parser
     class RSSEntry
-      element :"atom:summary", :as => :summary, :with => {:type => "text"}
+      element :"atom:summary", :as => :summary
     end
     
     class RSS
