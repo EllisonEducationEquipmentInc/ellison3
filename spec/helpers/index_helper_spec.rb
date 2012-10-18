7f5550587f553d2ef8450c20094020ab52ff2eea
Moises Vargas <moisesweb@gmail.com>
2012-10-18 09:25:49 -0500

    [37811247] locator title logic change

--- spec/helpers/index_helper_spec.rb
+++ spec/helpers/index_helper_spec.rb
@@ -3,16 +3,25 @@ require 'spec_helper'
 describe IndexHelper do
   describe "#store_locator_title" do
 
-    context "when current system locale is us" do
+    context "when current system is sizzix us" do
       it "returns the Store Locator title" do
-        helper.should_receive(:is_us?).and_return true
+        helper.should_receive(:is_sizzix_us?).and_return true
+        helper.store_locator_title.should eql "Store Locator"
+      end
+    end
+
+    context "when current system is ellison education us" do
+      it "returns the Store Locator title" do
+        helper.should_receive(:is_sizzix_us?).and_return false
+        helper.should_receive(:is_ee_us?).and_return true
         helper.store_locator_title.should eql "Store Locator"
       end
     end
 
     context "when current system locale is uk" do
       it "returns the Stockist List title" do
-        helper.should_receive(:is_us?).and_return false
+        helper.should_receive(:is_sizzix_us?).and_return false
+        helper.should_receive(:is_ee_us?).and_return false
         helper.should_receive(:is_uk?).and_return true
         helper.store_locator_title.should eql "Stockist List"
       end
@@ -20,7 +29,8 @@ describe IndexHelper do
 
     context "when current system locale is ellison retailer" do
       it "returns the Distributor Locator title" do
-        helper.should_receive(:is_us?).and_return false
+        helper.should_receive(:is_sizzix_us?).and_return false
+        helper.should_receive(:is_ee_us?).and_return false
         helper.should_receive(:is_uk?).and_return false
         helper.should_receive(:is_er?).and_return true
         helper.store_locator_title.should eql "Distributor Locator"
@@ -29,7 +39,8 @@ describe IndexHelper do
 
     context "when current system locale is not a Ellison Retailer and it's not from us or uk" do
       it "returns the Store Locator title" do
-        helper.should_receive(:is_us?).and_return false
+        helper.should_receive(:is_sizzix_us?).and_return false
+        helper.should_receive(:is_ee_us?).and_return false
         helper.should_receive(:is_uk?).and_return false
         helper.should_receive(:is_er?).and_return false
         helper.store_locator_title.should eql "Store Locator"
