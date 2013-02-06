b1ce0e90ed75b00c2812caef1e0ea12fb6b85008
Madhavi Barla <mbarla@test.com>
2013-02-06 09:41:24 -0800

    Reverting #1327 fixes and adding new fix as per Mark

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -12,6 +12,6 @@ class Feed
   validates_presence_of :name, :feeds
 
   def entries
-    ActiveSupport::JSON.decode(self.feeds.gsub(/\\u[^0]*[a-f0-9]{2,4}/, '')) rescue []
+    ActiveSupport::JSON.decode(self.feeds) rescue []
   end
 end
