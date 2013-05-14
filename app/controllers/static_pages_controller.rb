08d4a0657a33ce6f80666856ab0ba89c2b2bef4a
Mark Ronai <computadude@me.com>
2013-05-13 20:47:55 -0700

    fixes #1346 Nomethod error for static page controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,9 +8,10 @@ class StaticPagesController < ActionController::Metal
     begin
       render params[:path]
     rescue
+      Rails.logger.info "Wewe #{request.fullpath}"
       domain_to_system(env['HTTP_HOST'])
       set_current_system(params[:system]) if params[:system] && Rails.env == 'development'
-      render "#{Rails.root}/public/404_#{current_system}.html", :status => 404 unless request.fullpath =~ /\.[a-zA-Z0-9]{2,4}(\?\d{10})?$/
+      render "#{Rails.root}/public/404_#{current_system}.html", :status => 404 unless request.fullpath =~ /\.[a-zA-Z0-9]{2,4}(\?\d{10})?$/ && request.fullpath !~ /\.html?\Z/
     end
   end
 
