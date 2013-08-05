47b1f97a4aeb6b10acbcd6c67a39d7a396d766a4
Madhavi Barla <mbarla@test.com>
2013-08-05 16:29:39 -0700

    uncommitting last commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -266,7 +266,6 @@ class UsersController < ApplicationController
     if @address.address_type == 'shipping'
       get_cart.reset_tax_and_shipping(true)
       get_cart.update_items(false, false, @address.country)
-      calculate_shipping @address
     end
   end
 
