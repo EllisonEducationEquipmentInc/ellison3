cce4739b80ee9ec1ad835308dec07240b21db90a
Mark Ronai <computadude@me.com>
2013-10-19 13:01:08 -0700

    4. Upon user successful signed up from popup – thank you message is not displayed in popup

--- lib/lyrishq.rb
+++ lib/lyrishq.rb
@@ -53,7 +53,7 @@ class Lyrishq
 
   def error
     if error? && @response.present?
-      (@response/"data").inner_text == "Email address already exists" ? "Email address already exists in our list.  If you are not receiving our newsletter, please update your profile <a href='/newsletter-signup'>here</a> to start receiving the newsletter" : (@response/"data").inner_text
+      (@response/"data").inner_text == "Email address already exists" ? "Email address already exists in our list.  If you are not receiving our newsletter, please update your profile <a href='/newsletter-signup'>here</a> to start receiving the newsletter".html_safe : (@response/"data").inner_text
     end
   end
 
