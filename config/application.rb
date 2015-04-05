ff237b4d6c31bf0cdd978bde9544a774fa805137
Madhavi Barla <mbarla@test.com>
2015-04-05 15:49:27 -0700

    sass-rails gem

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require 'mongoid/railtie'
-#require "sprockets/railtie"
+require "sprockets/railtie"
 require 'rake'
 require 'csv'
 
